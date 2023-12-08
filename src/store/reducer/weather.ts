c985befd515f8e2a3c8a68e98d45b80c16c082e4
Hrach92 <hrach.gh.92@mail.ru>
2023-12-08 11:40:02 +0400

    changes

--- src/store/reducer/weather.ts
+++ src/store/reducer/weather.ts
@@ -2,19 +2,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const weather = createApi({
   reducerPath: "weather",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://api.weatherapi.com/v1/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://api.weatherapi.com/v1/" }),
   endpoints: (build) => ({
     findWeather: build.query({
       query: (text = "") =>
-        `current.json?key=87e0e8edb650486da83184302230712&q=${text}`,
+        `api/current.json?key=87e0e8edb650486da83184302230712&q=${text}`,
     }),
     findByIp: build.query({
       query: (ip = "") =>
-        `current.json?key=87e0e8edb650486da83184302230712&q=${ip}`,
+        `api/current.json?key=87e0e8edb650486da83184302230712&q=${ip}`,
     }),
     getDays: build.query({
       query: (text = "") =>
-        `forecast.json?key=87e0e8edb650486da83184302230712&q=${text}&days=7`,
+        `api/forecast.json?key=87e0e8edb650486da83184302230712&q=${text}&days=7`,
     }),
   }),
 });
